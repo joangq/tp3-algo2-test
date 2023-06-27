aaa2d94d1300c0e567c6f76db1338b242136b714
Joan Gonzalez <jgquiroga@dc.uba.ar>
2023-06-27 17:05:57 -0300

    Testeo que falle  GHActions

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -60,6 +60,10 @@ TEST(testsLolla, Primero) {
 	EXPECT_EQ(1,1);
 }
 
+TEST(failureTest, failHere) {
+	EXPECT_TRUE(false);
+}
+
 int main(int argc, char* argv[]) {
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
